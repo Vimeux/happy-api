99379f5a9551d7c5ba21975c9590e1708b083814
Benjamin Vimeux <vimeux.benji@gmail.com>
2021-12-11 15:56:31 +0100

    connection à la base de données

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
+require('dotenv').config() // sert pour récupérer les données d'un fichier .env
+
 const express = require('express')
+const mongoose = require('mongoose')
+// const cors = require('cors')
 
 const app = express()
-const port = 3111
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json()) // analyse les données entrante dans l'api, voir la doc express
+
+// const router = express.Router()
+
+const port = process.env.PORT
+
+const mongodbConnectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`
+
+// check si il y a une erreur de connection
+mongoose.connect(mongodbConnectionString, null, error => {
+  if (error) throw Error(error)
+})
+
+// connection à la base
+const db = mongoose.connection
+
+// ne sera éxécuté qu'une seule fois
+db.once('open', () => {
+  console.info('Connexion à la base: ok')
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World !')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${3111}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
