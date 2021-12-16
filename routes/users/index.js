00a93560f0c9c2e181fc48e0e985c834a448bbee
Benjamin Vimeux <vimeux.benji@gmail.com>
2021-12-16 21:54:57 +0100

    Abandon de l'update du user avec password

--- routes/users/index.js
+++ routes/users/index.js
@@ -24,18 +24,5 @@ router.route('/')
       .then(result => res.send('The user has been deleted'))
       .catch(error => res.status(500).send(error))
   })
-  // UPDATE
-  // .patch((req, res) => {
-  //   const id = extractIdFromRequestAuthHeader(req)
-  //   const { body: { user } } = req
-
-//   if (!user) return res.status(500).send('Missing Datas')
-
-//   User.findByIdAndUpdate(id, user, (error, result) => {
-//     if (error) return res.status(500).send(error)
-//     // Méthode Promesse en retirant le password des retournées par mongodb
-//     return res.send(result).select('-password')
-//   })
-// })
 
 module.exports = router
