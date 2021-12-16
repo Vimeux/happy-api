db42499cb1e92f4c42bbbbe80407200670142a0a
BenjaminZoan <benjamin@zoan.fr>
2021-12-16 17:01:50 +0100

    update user (probleme)

--- routes/users/index.js
+++ routes/users/index.js
@@ -5,8 +5,8 @@ const User = require('../../models/User')
 const withAuth = require('../../middlewares/authMiddleware')
 const { extractIdFromRequestAuthHeader } = require('../../helpers/TokenHelper')
 
-// Récupère et retourne un user par son id
 router.route('/')
+  // GET (Récupère et retourne un user par son id)
   .get(withAuth, (req, res) => {
     const id = extractIdFromRequestAuthHeader(req)
 
@@ -15,6 +15,7 @@ router.route('/')
       .then(result => res.send(result))
       .catch(error => res.status(500).send(error))
   })
+  // DELETE (Récupere et supprime un user par son id)
   .delete(withAuth, (req, res) => {
     const id = extractIdFromRequestAuthHeader(req)
 
@@ -23,5 +24,18 @@ router.route('/')
       .then(result => res.send('The user has been deleted'))
       .catch(error => res.status(500).send(error))
   })
+  // UPDATE
+  // .patch((req, res) => {
+  //   const id = extractIdFromRequestAuthHeader(req)
+  //   const { body: { user } } = req
+
+//   if (!user) return res.status(500).send('Missing Datas')
+
+//   User.findByIdAndUpdate(id, user, (error, result) => {
+//     if (error) return res.status(500).send(error)
+//     // Méthode Promesse en retirant le password des retournées par mongodb
+//     return res.send(result).select('-password')
+//   })
+// })
 
 module.exports = router
